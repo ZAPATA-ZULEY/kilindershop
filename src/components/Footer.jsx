44b270a00ca023f3caa5b0161a801082e7fe5285
david578 <elian.fernandez@correounivalle.edu.co>
2025-05-01 09:42:43 -0500

    inicio y creacion de proyecto en react

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+export function Footer () {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className='footer'>
+      <h4>kilindershop ⚛️ － <span>@kilindershop</span></h4>
+      <h5>Tienda virtual</h5>
+    </footer>
+  )
+}
